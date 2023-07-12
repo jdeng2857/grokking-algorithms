cfc3fbc44c0d25b616cd6fe3cdd0c41db0f9c868
jdeng2857 <jdeng2857@gmail.com>
2023-07-12 17:32:16 -0400

    Add bfs

---
+++ 6-BFS.py
@@ -0,0 +1,28 @@
+graph = {}
+graph["you"] = ["alice", "bob", "claire"]
+graph["bob"] = ["anuj", "peggy"]
+graph["alice"] = ["peggy"]
+graph["claire"] = ["thom", "jonny"]
+graph["anuj"] = []
+graph["peggy"] = []
+graph["thom"] = []
+graph["jonny"] = []
+
+from collections import deque
+
+def person_is_seller(name):
+  return name[-1] == 'm'
+
+def bfs():
+  search_queue = deque()
+  search_queue += graph["you"]
+  while search_queue:
+    person = search_queue.popleft()
+    if(person_is_seller(person)):
+      print(person + " is a mango seller!")
+      return True
+    else:
+      search_queue += graph[person]
+  return False
+
+print(bfs())
